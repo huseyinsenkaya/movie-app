707b6e964244c95eae016c950a0552d48291eca2
Hüseyin Sen Kaya <huseyinsenkaya@Huseyins-MacBook-Pro.local>
2023-09-27 04:43:57 +0300

    vite config base url change

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   return defineConfig({
+    base: "./",
     plugins: [vue()],
   });
 };
